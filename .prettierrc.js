f1c615c2e3e6fc2a1588196491989cdb83c3003a
Said-Ait-Driss <saidaitdrissofficial@gmail.com>
2024-08-27 11:27:30 +0100

    more ui

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     bracketSpacing: false,
     jsxBracketSameLine: true,
     tabWidth: 4,
-    printWidth: 150,
+    printWidth: 250,
     singleQuote: true,
     trailingComma: 'all',
     arrowParens: 'avoid',
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import brief from "../data/brief.json" assert { type: 'json' };
+import profile from "../data/profile.json" assert { type: 'json' };
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {brief});
+    res.render('index', {brief:profile.brief});
 });
+
+router.get("/about",(req,res)=>{
+    res.render("about",{...profile})
+})
+
+router.get("/work",(req,res)=>{
+    res.render("work",{brief:profile.brief,experiences:profile.experiences})
+})
+
 
 export default router;
