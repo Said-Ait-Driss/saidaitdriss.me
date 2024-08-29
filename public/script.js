d670fa67410199679fd5987f4d2a77dad5b47fb8
Said-Ait-Driss <saidaitdrissofficial@gmail.com>
2024-08-29 12:33:57 +0100

    more & more of ui :) ( adding mobile menu )

---
+++ public/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Loaded");
+
+    function initializeMenu() {
+        let menuBtn = document.getElementById("menu-btn");
+        let mobileMenu = document.getElementById("mobile-menu");
+        let main = document.querySelector("main");
+        let img = menuBtn.firstChild;
+
+        menuBtn.onclick = function(ev) {
+            if (mobileMenu.style.display == "block") {
+                mobileMenu.style.display = "none";
+                main.style.opacity = 1;
+                img.src = "/images/menu.svg";
+            } else {
+                mobileMenu.style.display = "block";
+                main.style.opacity = 0.2;
+                img.src = "/images/menu-closer.svg";
+            }
+        };
+
+        let pathname = window.location.pathname;
+
+        mobileMenu.querySelectorAll("a").forEach(function(link) {
+            if (link.getAttribute("hx-get") == pathname) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    }
+
+    // Initialize the menu on page load
+    initializeMenu();
+
+    // Reinitialize the menu after HTMX swaps content
+    document.body.addEventListener("htmx:afterSwap", function(event) {
+        initializeMenu();
+    });
+});
